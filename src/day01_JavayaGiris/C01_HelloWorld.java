853fe5515b6b64d3e239445d8ceb893516a3755c
nihalgokmen <nihalgokmen@icolud.com>
2023-05-24 16:26:56 +0300

    day01

---
+++ src/day01_JavayaGiris/C01_HelloWorld.java
@@ -0,0 +1,19 @@
+package day01_JavayaGiris;
+
+public class C01_HelloWorld {
+
+    public static void main(String[] args) {
+
+        // sout yazip enter'a basilinca konsolda azdirma kodu olan System.out.println cikar
+        // Java aciklama satirlarini IGNORE eder
+        System.out.println("Hello World");
+
+        /*
+            birden fazla
+            satirda comment
+            yapmak icin
+            kullanilan yontem
+         */
+        
+    }
+}
---
+++ src/day01_JavayaGiris/C02_Variables.java
@@ -0,0 +1,29 @@
+package day01_JavayaGiris;
+
+public class C02_Variables {
+
+    public static void main(String[] args) {
+
+        /*
+            Java RunTime programdir
+            yesil ok'a basip calistirdigimizda
+            hersey sifirdan baslar
+
+            class'in sonuna kadar tum kodlar
+            yukardan asagi calistirilir
+
+            class bittiginde hersey SIFIRLANIR
+         */
+
+        int bakiye=0;
+
+        bakiye=200;
+        System.out.println(bakiye); //200
+
+        bakiye=500;
+
+        bakiye=300;
+        System.out.println(bakiye); //300
+
+    }
+}
